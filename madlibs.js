6bad207ae3f22940daeddc9ec3a7bfc24adc74fe
Nida Ahmed <nahmed@cshnyc.org>
2018-11-07 14:46:30 -0500

    added file

---
+++ madlibs.js
@@ -0,0 +1,21 @@
+var noun1 = prompt("Imput a PLURAL Noun:")
+var noun2 = prompt("Imput a Noun:")
+var verb = prompt("Imput a Verb:")
+var number1 = prompt("Imput a number:")
+var number2 = prompt("Imput a number:")
+var total = number1 + number2
+var adjective1 = prompt("Imput an Adjective:")
+var adjective2 = prompt("Imput an Adjective:")
+var adjective3 = prompt("Imput an Adjective:")
+var adjective4 = prompt("Imput an Adjective:")
+var place1 = prompt("Imput a place:")
+var place2 = prompt("Imput a place:")
+var place3 = prompt("Imput a place:")
+
+document.write("Once upon a time there was a family of " + noun1 +". \
+<br>They went to the " + place1 + " to buy some " + noun2 + ".<br> \
+The " + noun2 + " was not very " + adjective1 +", so the " + noun1 + " had to go to another " + place2 + ". \
+At this " + place2 + ", the " + noun1 + " bought a load of " + adjective2 + " and " + adjective3 + " " + noun2 + ". \
+They went to the cashier, who told them that the first " + noun2 + " would be " + number1 + " dollars and the second " + noun2 + " would cost them " + number2 + " dollars. \
+In total they must pay " + total + " dollars for the " + noun2 + ". The " + noun1 + " did not want to pay this much for 2 " + noun2 + ", so they went to another " + place3 + ".\
+At this " + place3 +", the " + noun1 + " were finally able to purchase " + noun2 + " for a great price! The " + noun1 + " went home very " + adjective4 +". The End!")
